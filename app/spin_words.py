3b668382e22a71c929b71f3705485d870aa05019
Siyabonga Thwala <siyabongat@sahomeloans.com>
2020-01-14 11:37:41 +0200

    added katas

--- app/spin_words.py
+++ app/spin_words.py
@@ -1,14 +1,16 @@
 def spin_words(sentence:str):
+    print(" ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")]))
+'''    
     reversedString= ""
+
     for item in sentence.split():
         if len(item) >= 5:
-            reversedWord = ''.join(reversed(item))
-            reversedString =reversedString +' '+ reversedWord   
-            reversedString.replace(" ","")         
+            a = item[len(item)::-1]
+            reversedString += a + ' '
         else:
-            reversedString =item +' '+ reversedString
-            reversedString.replace(" ","") 
-             
-    print(reversedString) 
-
+            reversedString = reversedString +' '+ item + ' '
+           
+    #reversedString.replace("  "," ")
+    print((reversedString.strip()).replace("  "," "))
+'''
 spin_words('Hey fellow warriors')
