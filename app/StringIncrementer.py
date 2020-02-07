3d42741bc86801b26c12465b39f58e91fe3e42df
Siyabonga Thwala <siyabongat@sahomeloans.com>
2020-02-07 15:54:32 +0200

    added StringIncrement task

--- app/StringIncrementer.py
+++ app/StringIncrementer.py
@@ -1,8 +1,6 @@
 def increment_string(strng):
     if strng[len(strng)-1].isdigit():
-        # lastValue = strng[len(strng)-1]
         IncreasedlastValue =str(int(strng[len(strng)-1]) + 1)
-        # IncrementedString = strng.replace(strng[len(strng)-1],IncreasedlastValue)
         IncrementedString = strng[:-1] + IncreasedlastValue
     else:
          IncrementedString = strng + str(1)   
