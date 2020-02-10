3710ccf6475379696418fab59e006fc45ab57f4d
Siyabonga Thwala <siyabongat@sahomeloans.com>
2020-02-10 15:51:13 +0200

    added StringIncrement task

--- app/StringIncrementer.py
+++ app/StringIncrementer.py
@@ -1,9 +1,22 @@
+
 def increment_string(strng):
     if strng[len(strng)-1].isdigit():
-        IncreasedlastValue =str(int(strng[len(strng)-1]) + 1)
+        NumbersFromString=([int(item) for item in strng if item.isdigit()])
+        numbertoincrease= (''.join(map(str,NumbersFromString))) #joining the numbers from the string
+
+        IncreasedlastValue =str(int((''.join(map(str,NumbersFromString)))) + 1)
+        
         IncrementedString = strng[:-1] + IncreasedlastValue
     else:
          IncrementedString = strng + str(1)   
+        
+    print(IncrementedString) 
 
-    print(IncrementedString)     
-increment_string('foobar099')    
+    # NumbersFromString=([int(item) for item in strng if item.isdigit()])
+    # print(NumbersFromString)
+    # print(''.join(map(str,NumbersFromString)))
+    
+
+
+increment_string('foobar99')    
+    
